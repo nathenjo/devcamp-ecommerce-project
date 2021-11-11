a3ebe4531cab3960b6817525929978978148f84d
nathenjo <nateselectronicrepair@gmail.com>
2021-11-11 16:21:27 -0500

    Styled form fields components

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -25,9 +25,10 @@ class SignInForm extends Component {
                 placeholder='Password'
                 component={FormInput}
             />
+            <div className='sign-in-form__line'></div>
             <Field
                 className='sign-in-form__login'
-                onClick={() => console.log('Submit Click')}x
+                onClick={() => console.log('Submit Click')}
                 name='login'
                 type='submit'
                 title='Login'
