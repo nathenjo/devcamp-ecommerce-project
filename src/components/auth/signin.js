1b7a81c962d1c9279cb2ba3340b9d76f9d094f75
nathenjo <nateselectronicrepair@gmail.com>
2021-11-15 20:13:48 -0500

    Finished header feature

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -4,7 +4,15 @@ import { reduxForm, Field } from 'redux-form';
 import SignInForm from './signinForm';
 import PageTitle from '../pageTitle';
 
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
 class SignIn extends Component {
+
+  componentDidMount() {
+    this.props.setHeaderLinks([]);
+    this.props.setNavbarLinks([]);
+  }
 
     onSubmit = (fields) => {
       console.log(fields);
@@ -19,4 +27,7 @@ class SignIn extends Component {
        );
    }
 }
+
+SignIn = connect(null, actions)(SignIn);
+
 export default SignIn;
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -4,7 +4,15 @@ import { reduxForm, Field } from 'redux-form';
 import SignUpForm from './signupForm';
 import PageTitle from '../pageTitle';
 
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
 class SignUp extends Component {
+
+  componentDidMount() {
+    this.props.setHeaderLinks([]);
+    this.props.setNavbarLinks([]);
+  }
 
     onSubmit = (fields) => {
       console.log(fields);
@@ -19,4 +27,7 @@ class SignUp extends Component {
        );
    }
 }
+
+SignUp = connect(null, actions)(SignUp);
+
 export default SignUp;
