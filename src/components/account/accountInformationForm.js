5617d76814aa4f5a134c8a985683beac3610b15a
nathenjo <nateselectronicrepair@gmail.com>
2021-11-17 09:13:14 -0500

    Built account information form from signinform template

---
+++ src/components/account/accountInformationForm.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+
+import { reduxForm, Field } from 'redux-form';
+
+import { FormInput, FormButton } from '../formFields';
+import history from '../../history';
+
+class AccountInformationForm extends Component {
+     render() {
+         const { className, handleSubmit } = this.props;
+       return(
+         <form onSubmit={handleSubmit} className={`${className} 'sign-in-form`}>
+             <Field
+                className='sign-in-form__email'
+                name='email'
+                type='email'
+                title='Email'
+                placeholder='Email'
+                component={FormInput}
+            />
+            <Field
+                className='sign-in-form__password'
+                name='password'
+                type='password'
+                title='Password'
+                placeholder='Password'
+                component={FormInput}
+            />
+            <div className='sign-in-form__line'></div>
+            <Field
+                className='sign-in-form__login'
+                onClick={() => history.push('/account')}
+                name='login'
+                type='submit'
+                title='Login'
+                component={FormButton}
+            />
+         </form>
+       );
+   }
+}
+
+AccountInformationForm = reduxForm({
+  form: 'SignIn'
+})(AccountInformationForm);
+export default AccountInformationForm;
