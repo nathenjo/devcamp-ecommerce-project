c4e6bb59297c5cf6c9bf97014ec01e1ced579119
nathenjo <nateselectronicrepair@gmail.com>
2021-11-19 15:23:11 -0500

    Filtered products feature complete

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -35,7 +35,23 @@ class Shop extends Component {
        return(
          <div className='shop'>
              {/* shop searchbar component */}
-             {/* shop product component */}
+             <div className='shop__products'>
+                {
+                  this.props.filteredProducts.map(product => {
+                    return (
+                      <div key={product._id} className='shop-product'>
+                        <div className='shop-product__title'>
+                          {product.title}
+                        </div>
+
+                        <div className='shop-product__description'>
+                          {product.description}
+                        </div>
+                      </div>
+                    )
+                  })
+                }
+             </div>
              {/* shop cart button */}
              Shop
          </div>
@@ -44,9 +60,10 @@ class Shop extends Component {
 }
 
 function mapStateToProps(state) {
-  const { categories } = state.shop;
+  const { categories, filteredProducts } = state.shop;
     return { 
-      categories
+      categories,
+      filteredProducts
      }
 }
 
