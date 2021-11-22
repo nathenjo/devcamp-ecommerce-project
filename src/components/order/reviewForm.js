1dfd9390ee5f7ec1fe000fabf4f0c17f23c4cb1e
nathenjo <nateselectronicrepair@gmail.com>
2021-11-22 12:29:12 -0500

    Built review form buttons

---
+++ src/components/order/reviewForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+import { reduxForm, Field } from 'redux-form';
+
+import { FormButton } from '../formFields';
+import history from '../../history';
+
+class ReviewForm extends Component {
+     render() {
+         const { className, handleSubmit } = this.props;
+    
+       return(
+         <form onSubmit={handleSubmit} className={`${className} 'review-form`}>
+             
+            <div className='review-form__line'></div>
+            <Field
+                className='review-form__proceed'
+                onClick={() => history.push('/account')}
+                name='proceed'
+                type='submit'
+                title='Proceed to Checkout'
+                component={FormButton}
+            />
+            <Field
+                className='review-form__back'
+                onClick={() => history.push('/signin')}
+                name='back'
+                type='button'
+                title='Back'
+                component={FormButton}
+                short={true}
+            />
+         </form>
+       );
+   }
+}
+
+ReviewForm = reduxForm({
+  form: 'SignIn'
+})(ReviewForm);
+export default ReviewForm;
