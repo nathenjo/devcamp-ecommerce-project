c821bc23275d59ce44501a3de1c706ca89f2addd
nathenjo <nateselectronicrepair@gmail.com>
2021-11-22 13:14:36 -0500

    Built order review products component

--- src/components/order/reviewForm.js
+++ src/components/order/reviewForm.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-
 import { reduxForm, Field } from 'redux-form';
 
-import { FormButton } from '../formFields';
 import history from '../../history';
+
+import { FormButton } from '../formFields';
+import ReviewProducts from './reviewProducts';
 
 class ReviewForm extends Component {
      render() {
@@ -11,7 +12,9 @@ class ReviewForm extends Component {
     
        return(
          <form onSubmit={handleSubmit} className={`${className} 'review-form`}>
-             
+
+             <ReviewProducts className='review-form__products' />
+
             <div className='review-form__line'></div>
             <Field
                 className='review-form__proceed'
