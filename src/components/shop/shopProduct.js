e5269bc270244b17e0635d09f64f6cd7489b7003
nathenjo <nateselectronicrepair@gmail.com>
2021-11-22 14:57:28 -0500

    Built review product component

--- src/components/shop/shopProduct.js
+++ src/components/shop/shopProduct.js
@@ -12,8 +12,8 @@ class ShopProduct extends Component {
 
         if(document.getElementById('shop-cart').classList.contains('cart-hidden')) {
             document.getElementById('shop-cart').classList.remove('cart-hidden');
-            const { _id, title, description, price, belongsTo } = this.props;
-            this.props.addCartProduct({ _id, title, description, price, belongsTo });
+            const { _id, title, description, price, belongsTo, imageUrl } = this.props;
+            this.props.addCartProduct({ _id, title, description, price, belongsTo, imageUrl });
         } else {
             document.getElementById('shop-cart').classList.add('cart-hidden');
         }
