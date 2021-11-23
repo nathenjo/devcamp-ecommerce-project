60003e82ef1dd4a1385ac213d4cae68a0e82b219
nathenjo <nateselectronicrepair@gmail.com>
2021-11-23 13:03:44 -0500

    Built shipping to component

--- src/components/information/paymentForm.js
+++ src/components/information/paymentForm.js
@@ -5,6 +5,7 @@ import { reduxForm, Field } from 'redux-form';
 import { FormInput, FormButton } from '../formFields';
 import history from '../../history';
 import OrderSummary from './orderSummary';
+import { UnderlinedTitle } from './infoHelp';
 
 class PaymentForm extends Component {
      render() {
@@ -63,6 +64,11 @@ class PaymentForm extends Component {
                 short={true}
             />
             <OrderSummary className='payment-form__order-summary'/>
+            <div className='payment-form__shipping-info shipping-info'>
+                <UnderlinedTitle className='shipping-info__title' title='Shipping To' />
+                <div className='shipping-info__name small-text'>Jordan Hudgens</div>
+                <div className='shipping-info__address small-text'>123 Address Goes Here</div>
+            </div>
          </form>
        );
    }
